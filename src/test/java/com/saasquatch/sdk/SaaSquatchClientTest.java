55c6590af728adbb7036f8055e9648cf6cd1ca36
slisaasquatch <sli@saasquat.ch>
2019-09-30 14:47:52 -0700

    unit tests

---
+++ src/test/java/com/saasquatch/sdk/SaaSquatchClientTest.java
@@ -0,0 +1,15 @@
+package com.saasquatch.sdk;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class SaaSquatchClientTest {
+
+  @Test
+  public void testNull() {
+    assertThrows(NullPointerException.class, () -> SaaSquatchClient.createForTenant(null));
+    assertThrows(IllegalArgumentException.class, () -> SaaSquatchClient.createForTenant(" "));
+    assertThrows(NullPointerException.class, () -> SaaSquatchClient.create(null));
+  }
+
+}
