94388b5a8676b93677df799363f799d057feca17
slisaasquatch <sli@saasquat.ch>
2020-05-11 14:12:06 -0700

    separate out class

--- src/main/java/com/saasquatch/sdk/internal/json/GsonUtils.java
+++ src/main/java/com/saasquatch/sdk/internal/json/GsonUtils.java
@@ -1,22 +1,13 @@
 package com.saasquatch.sdk.internal.json;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 
 public final class GsonUtils {
 
@@ -50,39 +41,6 @@ public final class GsonUtils {
       jsonObject.remove(field);
     }
     return jsonObject.toString();
-  }
-
-  private static enum DateMillisAdapter implements JsonSerializer<Date>, JsonDeserializer<Date> {
-
-    INSTANCE;
-
-    @Override
-    public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext context) {
-      return new JsonPrimitive(src.getTime());
-    }
-
-    @Override
-    public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-        throws JsonParseException {
-      return new Date(json.getAsLong());
-    }
-
-  }
-
-  private static enum GsonIgnoreExlusionStrategy implements ExclusionStrategy {
-
-    INSTANCE;
-
-    @Override
-    public boolean shouldSkipField(FieldAttributes f) {
-      return f.getAnnotation(GsonIgnore.class) != null;
-    }
-
-    @Override
-    public boolean shouldSkipClass(Class<?> clazz) {
-      return clazz.getAnnotation(GsonIgnore.class) != null;
-    }
-
   }
 
 }
