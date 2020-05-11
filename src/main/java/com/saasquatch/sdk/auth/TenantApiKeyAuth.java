d4e8bbaea07eddabb66252c0472ca466fe6718ee
slisaasquatch <sli@saasquat.ch>
2020-05-11 15:53:57 -0700

    remove unnecessary base64 class

--- src/main/java/com/saasquatch/sdk/auth/TenantApiKeyAuth.java
+++ src/main/java/com/saasquatch/sdk/auth/TenantApiKeyAuth.java
@@ -1,9 +1,9 @@
 package com.saasquatch.sdk.auth;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
 import org.apache.hc.core5.http.HttpHeaders;
-import com.saasquatch.sdk.internal.OkioBase64;
 
 class TenantApiKeyAuth extends AuthMethod {
 
@@ -17,7 +17,7 @@ class TenantApiKeyAuth extends AuthMethod {
   @Override
   public void mutateRequest(SimpleHttpRequest request) {
     request.setHeader(HttpHeaders.AUTHORIZATION,
-        "Basic " + OkioBase64.encode((":" + apiKey).getBytes(UTF_8)));
+        "Basic " + Base64.encodeBase64String((":" + apiKey).getBytes(UTF_8)));
   }
 
 }
