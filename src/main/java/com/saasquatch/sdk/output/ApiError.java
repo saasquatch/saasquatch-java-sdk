75792e0ee65908d1bf49f8a3755d854d589cc59b
slisaasquatch <sli@saasquat.ch>
2020-05-11 12:17:48 -0700

    Update ApiError.java

--- src/main/java/com/saasquatch/sdk/output/ApiError.java
+++ src/main/java/com/saasquatch/sdk/output/ApiError.java
@@ -5,6 +5,9 @@ import static com.saasquatch.sdk.internal.InternalUtils.getBodyText;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * Represents an API error from SaaSquatch
@@ -46,7 +49,27 @@ public final class ApiError {
   }
 
   static ApiError fromResponse(SimpleHttpResponse response) {
-    return gson.fromJson(getBodyText(response), ApiError.class);
+    /*
+     * Doing this because in case of a catastrophic failure, we may not be able to get an actual
+     * ApiError from SaaSquatch.
+     */
+    final String bodyText = getBodyText(response);
+    final JsonElement jsonElement = JsonParser.parseString(bodyText);
+    /*
+     * If the response is a JSON object and it has the appropriate fields, then it's probably an
+     * actual ApiError.
+     */
+    if (jsonElement instanceof JsonObject) {
+      final JsonObject jsonObject = ((JsonObject) jsonElement);
+      if (jsonObject.has("message") && jsonObject.has("statusCode")) {
+        return gson.fromJson(jsonObject, ApiError.class);
+      }
+    }
+    /*
+     * This is a catastrophic failure and SaaSquatch servers failed to respond with a proper
+     * ApiError. Just jam the response text into the error message.
+     */
+    return new ApiError(bodyText, "UNHANDLED_API_EXCEPTION", response.getCode(), null);
   }
 
 }
