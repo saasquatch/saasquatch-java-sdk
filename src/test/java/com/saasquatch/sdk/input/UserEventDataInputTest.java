a35a75337d0d1c97efd23b10fa0f6a5a2d412a89
slisaasquatch <sli@saasquat.ch>
2020-05-12 13:40:54 -0700

    Update UserEventDataInputTest.java

--- src/test/java/com/saasquatch/sdk/input/UserEventDataInputTest.java
+++ src/test/java/com/saasquatch/sdk/input/UserEventDataInputTest.java
@@ -1,7 +1,10 @@
 package com.saasquatch.sdk.input;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Collections;
+import java.util.Date;
 import org.junit.jupiter.api.Test;
 
 public class UserEventDataInputTest {
@@ -12,6 +15,15 @@ public class UserEventDataInputTest {
     assertThrows(IllegalArgumentException.class,
         () -> UserEventDataInput.newBuilder().setKey(" ").build());
     assertDoesNotThrow(() -> UserEventDataInput.newBuilder().setKey("foo").build());
+  }
+
+  @Test
+  public void testBasic() {
+    final UserEventDataInput userEventDataInput = UserEventDataInput.newBuilder().setKey("key")
+        .setDateTriggered(new Date(123)).setFields(Collections.singletonMap("foo", "bar")).build();
+    assertEquals("key", userEventDataInput.getKey());
+    assertEquals(new Date(123), userEventDataInput.getDateTriggered());
+    assertEquals(Collections.singletonMap("foo", "bar"), userEventDataInput.getFields());
   }
 
 }
