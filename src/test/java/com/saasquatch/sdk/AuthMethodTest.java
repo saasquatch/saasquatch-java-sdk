e2200e93591602304067d414edcac0cc240531c3
slisaasquatch <sli@saasquat.ch>
2020-05-12 09:21:12 -0700

    Update AuthMethodTest.java

--- src/test/java/com/saasquatch/sdk/AuthMethodTest.java
+++ src/test/java/com/saasquatch/sdk/AuthMethodTest.java
@@ -1,5 +1,6 @@
 package com.saasquatch.sdk;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
@@ -8,9 +9,16 @@ import com.saasquatch.sdk.auth.AuthMethods;
 public class AuthMethodTest {
 
   @Test
-  public void testNull() {
+  public void testValidation() {
     assertThrows(NullPointerException.class, () -> AuthMethods.ofTenantApiKey(null));
+    assertThrows(IllegalArgumentException.class, () -> AuthMethods.ofTenantApiKey(" "));
+    assertThrows(NullPointerException.class, () -> AuthMethods.ofBasic(null, "abc"));
+    assertThrows(NullPointerException.class, () -> AuthMethods.ofBasic("abc", null));
+    assertDoesNotThrow(() -> AuthMethods.ofBasic("", ""));
     assertThrows(NullPointerException.class, () -> AuthMethods.ofJwt(null));
+    assertThrows(IllegalArgumentException.class, () -> AuthMethods.ofJwt(" "));
+    assertThrows(NullPointerException.class, () -> AuthMethods.ofBearer(null));
+    assertThrows(IllegalArgumentException.class, () -> AuthMethods.ofBearer(" "));
   }
 
   @Test
