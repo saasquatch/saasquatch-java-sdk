9e23e19611705eaa507bff9913007a4302ff7e94
slisaasquatch <sli@saasquat.ch>
2020-05-12 10:23:42 -0700

    Update UserLinkInputTest.java

--- src/test/java/com/saasquatch/sdk/input/UserLinkInputTest.java
+++ src/test/java/com/saasquatch/sdk/input/UserLinkInputTest.java
@@ -1,5 +1,6 @@
 package com.saasquatch.sdk.input;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,17 @@ public class UserLinkInputTest {
         () -> UserLinkInput.newBuilder().setAccountId("a").build());
     assertThrows(NullPointerException.class,
         () -> UserLinkInput.newBuilder().setUserId("a").build());
+  }
+
+  @Test
+  public void testBasic() {
+    final UserLinkInput userLinkInput = UserLinkInput.newBuilder().setAccountId("a").setUserId("u")
+        .setProgramId("p").setEngagementMedium("EMAIL").setShareMedium("FACEBOOK").build();
+    assertEquals("a", userLinkInput.getAccountId());
+    assertEquals("u", userLinkInput.getUserId());
+    assertEquals("p", userLinkInput.getProgramId());
+    assertEquals("EMAIL", userLinkInput.getEngagementMedium());
+    assertEquals("FACEBOOK", userLinkInput.getShareMedium());
   }
 
 }
