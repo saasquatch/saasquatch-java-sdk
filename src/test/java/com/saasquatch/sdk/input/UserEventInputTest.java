f83c2804a3652bac699e44300c5a1407f48ee067
slisaasquatch <sli@saasquat.ch>
2020-05-12 13:50:46 -0700

    Update UserEventInputTest.java

--- src/test/java/com/saasquatch/sdk/input/UserEventInputTest.java
+++ src/test/java/com/saasquatch/sdk/input/UserEventInputTest.java
@@ -1,6 +1,8 @@
 package com.saasquatch.sdk.input;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class UserEventInputTest {
@@ -14,6 +16,16 @@ public class UserEventInputTest {
         .setUserId("a").addEvents(UserEventDataInput.newBuilder().build()).build());
     assertThrows(NullPointerException.class, () -> UserEventInput.newBuilder()
         .addEvents(UserEventDataInput.newBuilder().setKey("foo").build()).build());
+  }
+
+  @Test
+  public void testBasic() {
+    final UserEventInput userEventInput = UserEventInput.newBuilder().setAccountId("a")
+        .setUserId("u")
+        .setEvents(Arrays.asList(UserEventDataInput.newBuilder().setKey("key").build())).build();
+    assertEquals("a", userEventInput.getAccountId());
+    assertEquals("u", userEventInput.getUserId());
+    assertEquals(1, userEventInput.getEvents().size());
   }
 
 }
