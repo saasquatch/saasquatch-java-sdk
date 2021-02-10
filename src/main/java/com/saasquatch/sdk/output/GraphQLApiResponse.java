5ead402d6ee7b4028072e71866da6ab0aec36c1a
slisaasquatch <sli@saasquat.ch>
2021-02-09 19:00:06 -0800

    Update GraphQLApiResponse.java

--- src/main/java/com/saasquatch/sdk/output/GraphQLApiResponse.java
+++ src/main/java/com/saasquatch/sdk/output/GraphQLApiResponse.java
@@ -7,9 +7,8 @@ import com.saasquatch.sdk.internal.json.GsonUtils;
 /**
  * Response from a GraphQL request. Note that this class differs from other {@link ApiResponse}
  * implementations in that even if the request causes an error, the response will still usually
- * {@link #succeeded() succeed}. If the the request has actually {@link #failed()}, then it's
- * usually a catastrophic failure. To get the actual error (and data), use {@link #getData()} and
- * access it from {@link GraphQLResult}.
+ * succeed. If the the request has actually failed, then it's usually a catastrophic failure. To get
+ * the actual error (and data), use {@link #getData()} and access it from {@link GraphQLResult}.
  *
  * @author sli
  * @see GraphQLResult
