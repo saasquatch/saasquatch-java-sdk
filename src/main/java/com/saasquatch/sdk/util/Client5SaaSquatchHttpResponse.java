e5065a5f7f85c64273223c8db7291fe06c537490
slisaasquatch <sli@saasquat.ch>
2021-02-10 17:57:20 -0800

    Make http response headers immutable

--- src/main/java/com/saasquatch/sdk/util/Client5SaaSquatchHttpResponse.java
+++ src/main/java/com/saasquatch/sdk/util/Client5SaaSquatchHttpResponse.java
@@ -10,8 +10,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
 import org.apache.hc.core5.http.Header;
 
@@ -20,6 +18,8 @@ public final class Client5SaaSquatchHttpResponse implements SaaSquatchHttpRespon
 
   // Lazy init
   private String bodyText;
+  // Lazy init
+  private Map<String, List<String>> allHeaders;
   private final SimpleHttpResponse response;
 
   @Internal
@@ -29,11 +29,11 @@ public final class Client5SaaSquatchHttpResponse implements SaaSquatchHttpRespon
 
   @Override
   public String getBodyText() {
-    String s = bodyText;
-    if (s == null) {
-      bodyText = s = InternalUtils.getBodyText(response);
+    String _bodyText = bodyText;
+    if (_bodyText == null) {
+      bodyText = _bodyText = InternalUtils.getBodyText(response);
     }
-    return s;
+    return _bodyText;
   }
 
   @Override
@@ -41,45 +41,34 @@ public final class Client5SaaSquatchHttpResponse implements SaaSquatchHttpRespon
     return response.getCode();
   }
 
-  @Nonnull
-  @Override
-  public final List<String> getHeaders(String headerName) {
-    final List<String> result = new ArrayList<>();
-    final Iterator<Header> headerIterator = response.headerIterator(headerName);
-    while (headerIterator.hasNext()) {
-      result.add(headerIterator.next().getValue());
-    }
-    return unmodifiableList(result);
-  }
-
-  @Nullable
-  @Override
-  public final String getFirstHeader(String headerName) {
-    final Header header = response.getFirstHeader(headerName);
-    return header == null ? null : header.getValue();
-  }
-
-  @Nullable
-  @Override
-  public final String getLastHeader(String headerName) {
-    final Header header = response.getLastHeader(headerName);
-    return header == null ? null : header.getValue();
-  }
-
   @Override
   public final Map<String, List<String>> getAllHeaders() {
+    Map<String, List<String>> _allHeaders = allHeaders;
+    if (_allHeaders == null) {
+      allHeaders = _allHeaders = _getAllHeaders();
+    }
+    return _allHeaders;
+  }
+
+  private Map<String, List<String>> _getAllHeaders() {
     final Map<String, List<String>> result = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
     final Iterator<Header> headerIterator = response.headerIterator();
     while (headerIterator.hasNext()) {
       final Header header = headerIterator.next();
       List<String> values = result.get(header.getName());
+      //noinspection Java8MapApi
       if (values == null) {
         values = new ArrayList<>();
         result.put(header.getName(), values);
       }
       values.add(header.getValue());
     }
-    return Collections.unmodifiableMap(result);
+    final Map<String, List<String>> resultCopy = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    for (Map.Entry<String, List<String>> e : result.entrySet()) {
+      resultCopy.put(e.getKey(), unmodifiableList(e.getValue()));
+    }
+    // DO NOT use InternalUtils.unmodifiableMap
+    return Collections.unmodifiableMap(resultCopy);
   }
 
 }
--- src/main/java/com/saasquatch/sdk/util/SaaSquatchHttpResponse.java
+++ src/main/java/com/saasquatch/sdk/util/SaaSquatchHttpResponse.java
@@ -1,6 +1,7 @@
 package com.saasquatch.sdk.util;
 
 import com.saasquatch.sdk.annotations.NoExternalImpl;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nonnull;
@@ -25,25 +26,36 @@ public interface SaaSquatchHttpResponse {
   int getStatusCode();
 
   /**
-   * @return All the header values for the given header name
+   * @return All the header values for the given header name. The returned {@link List} should be
+   * immutable.
    */
   @Nonnull
-  List<String> getHeaders(String headerName);
+  default List<String> getHeaders(String headerName) {
+    final List<String> headers = getAllHeaders().get(headerName);
+    return headers == null ? Collections.emptyList() : headers;
+  }
 
   /**
    * @return The first header value for the given header name
    */
   @Nullable
-  String getFirstHeader(String headerName);
+  default String getFirstHeader(String headerName) {
+    final List<String> headers = getHeaders(headerName);
+    return headers.isEmpty() ? null : headers.get(0);
+  }
 
   /**
    * @return The last header value for the given header name
    */
   @Nullable
-  String getLastHeader(String headerName);
+  default String getLastHeader(String headerName) {
+    final List<String> headers = getHeaders(headerName);
+    return headers.isEmpty() ? null : headers.get(headers.size() - 1);
+  }
 
   /**
-   * @return All the headers
+   * @return All the headers. The returned {@link Map} and its values should be immutable and the
+   * keys are case-insensitive.
    */
   Map<String, List<String>> getAllHeaders();
 
