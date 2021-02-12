eaee1f2de7f19682ecfaa523e03d9efc140c8f29
slisaasquatch <sli@saasquat.ch>
2021-02-11 20:13:05 -0800

    input type test

--- src/test/java/com/saasquatch/sdk/input/GetUserLinkInputTest.java
+++ src/test/java/com/saasquatch/sdk/input/GetUserLinkInputTest.java
@@ -2,6 +2,7 @@ package com.saasquatch.sdk.input;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class GetUserLinkInputTest {
@@ -17,8 +18,9 @@ public class GetUserLinkInputTest {
 
   @Test
   public void testBasic() {
-    final GetUserLinkInput userLinkInput = GetUserLinkInput.newBuilder().setAccountId("a").setUserId("u")
-        .setProgramId("p").setEngagementMedium("EMAIL").setShareMedium("FACEBOOK").build();
+    final GetUserLinkInput userLinkInput = GetUserLinkInput.newBuilder()
+        .setAccountId("a").setUserId("u").setProgramId("p")
+        .setEngagementMedium("EMAIL").setShareMedium("FACEBOOK").build();
     assertEquals("a", userLinkInput.getAccountId());
     assertEquals("u", userLinkInput.getUserId());
     assertEquals("p", userLinkInput.getProgramId());
