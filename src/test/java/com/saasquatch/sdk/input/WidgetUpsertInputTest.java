9a073f92b1dbe8478850e7f9351f0e24ef5a4dae
slisaasquatch <sli@saasquat.ch>
2021-02-11 20:13:13 -0800

    Update WidgetUpsertInputTest.java

--- src/test/java/com/saasquatch/sdk/input/WidgetUpsertInputTest.java
+++ src/test/java/com/saasquatch/sdk/input/WidgetUpsertInputTest.java
@@ -3,7 +3,6 @@ package com.saasquatch.sdk.input;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.google.common.collect.ImmutableMap;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WidgetUpsertInputTest {
