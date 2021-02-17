6b33facef40c7861306366167ef2037d3da76a86
slisaasquatch <sli@saasquat.ch>
2021-02-16 16:52:06 -0800

    Update SaaSquatchHttpResponseTest.java

--- src/test/java/com/saasquatch/sdk/http/SaaSquatchHttpResponseTest.java
+++ src/test/java/com/saasquatch/sdk/http/SaaSquatchHttpResponseTest.java
@@ -1,12 +1,14 @@
 package com.saasquatch.sdk.http;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.google.common.collect.ImmutableMap;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.zip.GZIPOutputStream;
 import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SaaSquatchHttpResponseTest {
@@ -24,6 +26,22 @@ public class SaaSquatchHttpResponseTest {
     assertEquals(ImmutableMap.of("abc", Arrays.asList("1", "2", "3")), response.getAllHeaders());
     assertEquals(200, response.getStatusCode());
     assertEquals("foo", response.getBodyText());
+  }
+
+  @Test
+  public void testGzip() throws Exception {
+    final String bodyText = "456489489624862476894786478652786527865427864127864176878621786";
+    final byte[] gzipBytes;
+    try (ByteArrayOutputStream baOut = new ByteArrayOutputStream()) {
+      try (GZIPOutputStream gzipOut = new GZIPOutputStream(baOut)) {
+        gzipOut.write(bodyText.getBytes(UTF_8));
+      }
+      gzipBytes = baOut.toByteArray();
+    }
+    final SimpleHttpResponse r1 = SimpleHttpResponse.create(400, gzipBytes);
+    r1.addHeader("content-encoding", "gzip");
+    final SaaSquatchHttpResponse response = new Client5SaaSquatchHttpResponse(r1);
+    assertEquals(bodyText, response.getBodyText());
   }
 
 }
