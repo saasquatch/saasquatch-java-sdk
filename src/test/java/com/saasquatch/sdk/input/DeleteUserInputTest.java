ec196d02263d03ac32f8dd54ee784c04ef4cf2bf
slisaasquatch <sli@saasquat.ch>
2021-02-24 15:46:55 -0800

    Create DeleteUserInputTest.java

---
+++ src/test/java/com/saasquatch/sdk/input/DeleteUserInputTest.java
@@ -0,0 +1,29 @@
+package com.saasquatch.sdk.input;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class DeleteUserInputTest {
+
+  @Test
+  public void testValidation() {
+    assertThrows(NullPointerException.class, () -> DeleteUserInput.newBuilder().build());
+    assertThrows(NullPointerException.class,
+        () -> DeleteUserInput.newBuilder().setUserId("a").build());
+    assertThrows(NullPointerException.class,
+        () -> DeleteUserInput.newBuilder().setAccountId("a").build());
+  }
+
+  @Test
+  public void testBasic() {
+    assertFalse(
+        DeleteUserInput.newBuilder().setAccountId("a").setUserId("a").build().isDoNotTrack());
+    assertTrue(
+        DeleteUserInput.newBuilder().setAccountId("a").setUserId("a").setDoNotTrack(true).build()
+            .isDoNotTrack());
+  }
+
+}
