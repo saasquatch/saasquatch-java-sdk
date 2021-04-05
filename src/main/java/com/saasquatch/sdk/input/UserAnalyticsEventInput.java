0bb8073db4d132a2d9428bc85dd6422c4db4cc3b
slisaasquatch <sli@saasquat.ch>
2021-04-05 10:54:48 -0700

    Update UserAnalyticsEventInput.java

--- src/main/java/com/saasquatch/sdk/input/UserAnalyticsEventInput.java
+++ src/main/java/com/saasquatch/sdk/input/UserAnalyticsEventInput.java
@@ -1,8 +1,10 @@
 package com.saasquatch.sdk.input;
 
+import static com.saasquatch.sdk.internal.InternalUtils.getUserIdInputFromUserJwt;
 import static com.saasquatch.sdk.internal.InternalUtils.requireNotBlank;
 
 import com.saasquatch.sdk.annotations.Internal;
+import com.saasquatch.sdk.internal.json.GsonIgnore;
 import java.util.Map;
 import java.util.Objects;
 import javax.annotation.Nonnull;
@@ -11,12 +13,14 @@ public final class UserAnalyticsEventInput {
 
   private final String id;
   private final String accountId;
+  @GsonIgnore
   private final String userJwt;
   private final String programId;
   private final String type;
   private final Map<String, Object> meta;
 
-  private UserAnalyticsEventInput(String id, String accountId, String userJwt, String programId, String type, Map<String, Object> meta) {
+  private UserAnalyticsEventInput(String id, String accountId, String userJwt, String programId,
+      String type, Map<String, Object> meta) {
     this.id = id;
     this.accountId = accountId;
     this.userJwt = userJwt;
@@ -56,8 +60,7 @@ public final class UserAnalyticsEventInput {
 
   public static final class Builder {
 
-    private String id;
-    private String accountId;
+    private UserIdInput user;
     private String userJwt;
     private String programId;
     private String type;
@@ -65,13 +68,21 @@ public final class UserAnalyticsEventInput {
 
     private Builder() {}
 
-    public Builder setId(@Nonnull String id) {
-      this.id = requireNotBlank(id, "id");
+    /**
+     * Set the user IDs with a {@link UserIdInput}
+     */
+    public Builder setUser(@Nonnull UserIdInput user) {
+      this.user = Objects.requireNonNull(user, "user");
       return this;
     }
 
-    public Builder setAccountId(@Nonnull String accountId) {
-      this.accountId = requireNotBlank(accountId, "accountId");
+    /**
+     * Convenience method for {@link #setUser(UserIdInput)} that accepts a user JWT
+     */
+    public Builder setUserWithUserJwt(@Nonnull String userJwt) {
+      requireNotBlank(userJwt, "userJwt");
+      this.user = getUserIdInputFromUserJwt(userJwt);
+      this.userJwt = userJwt;
       return this;
     }
 
@@ -96,7 +107,8 @@ public final class UserAnalyticsEventInput {
     }
 
     public UserAnalyticsEventInput build() {
-      return new UserAnalyticsEventInput(id, accountId, userJwt, programId, type, meta);
+      return new UserAnalyticsEventInput(user.getId(), user.getAccountId(), userJwt, programId,
+          type, meta);
     }
 
   }
